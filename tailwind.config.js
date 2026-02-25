6d39c454ab5270e8d935042557cc61e197b9585d
Micho Calma <34332755+webdevMLC@users.noreply.github.com>
2026-02-25 17:22:45 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+      colors: {
+        'gray-850': 'rgb(30, 41, 59)',
+      },
+    },
+  },
+  plugins: [],
+};
